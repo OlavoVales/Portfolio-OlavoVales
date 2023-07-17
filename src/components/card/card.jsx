fd3d5ab836e0c30303d0181cd479311ee1f3a3f2
OlavoVales <olavo345@gmail.com>
2023-07-17 10:27:00 -0300

    mudança nomenclatura pra deploy netlify

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,4 +1,4 @@
-import styles from './Card.module.css'
+import styles from './card.module.css'
 import { FaHtml5, FaCss3Alt, FaJs, FaReact } from 'react-icons/fa'
 import { BsArrowRight } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
